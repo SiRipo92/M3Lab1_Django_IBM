1c6c0b0907829970aa99fb59ed83b5865142b96e
SiRipo92 <ripochesierra@gmail.com>
2024-12-12 07:32:18 -0500

    Added my first view

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # Create a simple html page as a string
+    template = "<html>" \
+                "This is your first view" \
+               "</html>"
+    # Return the template as content argument in HTTP response
+    return HttpResponse(content=template)
